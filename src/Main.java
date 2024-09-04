4a32ac99c3c8bd3872ca293b51bcac2cfe94bd85
Robertron624 <robert.624@hotmail.com>
2024-09-04 10:14:06 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    static Integer userAge = 22;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("The user's age is: " + userAge);
+    }
+}
